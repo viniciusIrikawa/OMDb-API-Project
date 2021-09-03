4d3305e5701a81805dc2f674cc2541b8de591470
unknown <viniciusiandrade@hotmail.com>
2021-09-03 16:40:15 -0300

    v0.2

--- OMDb API/script.js
+++ OMDb API/script.js
@@ -1,9 +1,12 @@
 function search(){
+    const title = document.getElementById('movie-title').value;
     fetch(`http://www.omdbapi.com/?t=${title}&apikey=dbd7825d`)
         .then((response) => response.json())
         .then((dados) => {
-            // const title = document.getElementById('movie-title').value;
-            console.log(dados.title);
+            // console.log(dados);
+            console.log(dados.Actors);
+            console.log(dados.Title);
+            console.log(dados.Genre);
         });
 }
     
