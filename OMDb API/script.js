f49aaa89a7fcb2142faa06d335c413df905cd328
unknown <viniciusiandrade@hotmail.com>
2021-09-06 17:49:09 -0300

    Responsivity v.01

--- OMDb API/script.js
+++ OMDb API/script.js
@@ -1,24 +1,25 @@
 function search(){
     const inputTitle = document.getElementById('movie-title').value;
     fetch(`http://www.omdbapi.com/?t=${inputTitle}&apikey=dbd7825d`)
-        .then((response) => response.json())
-        .then((dados) => {
-            console.log(dados);
-            console.log('Capa do filme: ' + dados.Poster);
-
-            const image = document.getElementById('image');
-            image.setAttribute('src' , dados.Poster);
-            
-            const title = document.getElementById('content-title');
-            title.innerHTML = dados.Title;
-            
-            const year = document.getElementById('content-year');
-            year.innerHTML = dados.Year;
-            
-            const contentDescription = document.getElementById('content-description');
-            contentDescription.innerHTML = dados.Plot;
-        });
-    }
+    .then((response) => response.json())
+    .then((dados) => {
+        // console.log(dados);
+        // console.log('Capa do filme: ' + dados.Poster);
+        
+        const image = document.getElementById('image');
+        image.setAttribute('src' , dados.Poster);
+        
+        const title = document.getElementById('content-title');
+        title.innerHTML = dados.Title;
+        
+        const year = document.getElementById('content-year');
+        year.innerHTML = dados.Year;
+        
+        const contentDescription = document.getElementById('content-description');
+        contentDescription.innerHTML = dados.Plot;
+    })
+    .catch(error => console.log(error.response));
+}
 
 
 //Key enter listener
